0d828d3cd3c978817040fbe9096ae86503c1e03b
Artur Sales <161485148+artursales11@users.noreply.github.com>
2024-09-10 15:23:25 -0300

    Add files via upload
    ESTUDO DE WHILE LOOPING E IDADE

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let numero = prompt("Digite um número:");
+
+numero = parseInt(numero);
+
+
+let resultado = (numero % 2 === 0) ? "O número é par" : "O número é ímpar";
+
+const n1 = 5
+const n2 = "5"
+const n3 = 5
+
+console.log(n1 == n2)
