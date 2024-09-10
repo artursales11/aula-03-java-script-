437a3c4cb79c25e18b337a434ccdd120d19ae9d0
Artur Sales <161485148+artursales11@users.noreply.github.com>
2024-09-10 15:23:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
+function getDayOfWeek(number) {
+    let day;
+    
+    switch (number) {
+        case 1:
+            alert = "Domingo";
+            break;
+        case 2:
+            alert = "Segunda-feira";
+            break;
+        case 3:
+            alert = "Terça-feira";
+            break;
+        case 4:
+            alert = "Quarta-feira";
+            break;
+        case 5:
+            alert = "Quinta-feira";
+            break;
+        case 6:
+            alert = "Sexta-feira";
+            break;
+        case 7:
+            alert= "Sábado";
+            break;
+        default:
+            day = "Número inválido! Por favor, insira um número de 1 a 7.";
+    }
+    
+    alert(day);
+}
 
-let numero = prompt("Digite um número:");
-
-numero = parseInt(numero);
 
 
-let resultado = (numero % 2 === 0) ? "O número é par" : "O número é ímpar";
-
-const n1 = 5
-const n2 = "5"
-const n3 = 5
-
-console.log(n1 == n2)
